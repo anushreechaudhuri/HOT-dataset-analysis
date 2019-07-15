731eb443c4cef810ba3d7921e93936c71c22a48e
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2019-07-15 07:40:27 -0700

    Got LSA CSV formatting working :)

--- test.py
+++ test.py
@@ -5,24 +5,30 @@ import argparse
 pd.options.mode.chained_assignment = None
 
 parser = argparse.ArgumentParser(description='test file for df')
-parser.add_argument('num_bins', action='store_true', default=2, help='number of pressure bins to use') # that sets the default argument to 2
+parser.add_argument('-num_bins', default=2, help='number of pressure bins to use', type=int) # that sets the default argument to 2
+parser.add_argument('-maxp', default=1200, help='maximum pressure to include', type=int) # that sets the default argument to 2
 args = parser.parse_args()
 
 df_unfiltered = pd.read_pickle('WATER_DATA_FULL.pkl')
 df = df_unfiltered.replace(-9.0, np.nan)
 df['CTDPRS'] = df['CTDPRS'].mask(df['CTDPRS'] < 0, np.nan)
-df['CTDPRS'] = df['CTDPRS'].mask(df['CTDPRS'] > 2000, np.nan)
+df['CTDPRS'] = df['CTDPRS'].mask(df['CTDPRS'] > args.maxp, np.nan)
 df = df.drop(df.loc[df['CTDPRS'].isnull()].index)
-df['pbin'] = df['CTDPRS'] // (2000 / args.num_bins)
-
-for i in range(args.num_bins)
-  cruise1 = df.loc[df['CRUISENO'] == 1]
-  cruise1 = cruise1.copy()
+df['pbin'] = df['CTDPRS'] // (args.maxp / args.num_bins)
 
 
+time_axis = []
+for c in range(1, max(df['CRUISENO'])):
+  cruise = df.loc[df['CRUISENO'] == c] #.copy()
+  cruise_list = []
+  for b in range(args.num_bins):
+    bin = cruise.loc[cruise['pbin'] == b].drop(columns=['pbin']).mean(skipna=True).add_suffix(f"bin{b}")
+    cruise_list.append(bin)
+  time_axis.append(pd.concat(cruise_list))
+
+pd.concat(time_axis, axis=1).to_csv('lsa.csv')
+  
 # cruise1['pbin'] = cruise1.apply(lambda row: row['CTDPRS'] // (2000/args.num_bins), axis=1)
-bin1 = cruise1.loc[cruise1['pbin'] == 0] # get first pressure bin
-print(bin1.mean(skipna=True))
 
 # time_series = 
 
